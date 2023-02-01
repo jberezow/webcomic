a6124026073eaa9bbc897217507f10c158baa176
jberezow <jberez@live.ca>
2023-02-01 20:32:24 +0100

    Basic comic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Comic } from './comic';
-import yellowBuckets from '../comics/images/yellowBuckets.png'
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { Comic } from "./comic";
+import yellowBuckets from "./assets/comics/images/yellowBuckets.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <Comic image={yellowBuckets} alt="Weber Smash" caption="The Predators incite controversy by electing to swap navy blue helmets for golden." />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Comic
+					image={yellowBuckets}
+					alt="Weber Smash"
+					caption="The Predators incite controversy by electing to swap navy blue helmets for golden."
+				/>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/comic.tsx
+++ src/comic.tsx
@@ -1,17 +1,17 @@
-import React, { FC } from 'react';
-import { StyledComic } from './StyledComic';
+import React, { FC } from "react";
+import { StyledComic } from "./StyledComic";
 
 interface Props {
-  image: string;
-  alt: string;
-  caption: string;
+	image: string;
+	alt: string;
+	caption: string;
 }
 
 export const Comic: FC<Props> = ({ image, alt, caption }) => {
-  return (
-    <StyledComic>
-      <img src={image} alt={alt} />
-      <p>{caption}</p>
-    </StyledComic>
-  );
+	return (
+		<StyledComic>
+			<img src={image} alt={alt} />
+			<p>{caption}</p>
+		</StyledComic>
+	);
 };
