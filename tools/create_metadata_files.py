d3c2b19a416a5fc26cfebbd103ae988a9ad662c4
jberezow <jberez@live.ca>
2023-02-05 18:06:01 +0100

    Update tool for creating metadata files

--- tools/create_metadata_files.py
+++ tools/create_metadata_files.py
@@ -2,8 +2,15 @@ import json
 import argparse
 import glob
 import os
+import time
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from IPython import display
 
 from comic import Comic
+
 
 def main(source_folder: str, target_folder: str, force: bool) -> None:
   print(f'Creating metadata files for {source_folder} and saving to {target_folder}')
@@ -12,7 +19,7 @@ def main(source_folder: str, target_folder: str, force: bool) -> None:
   comics = glob.glob(f'{source_folder}/*.png') + glob.glob(f'{source_folder}/*.jpg')
 
   for comic in comics:
-    comic_handle = comic.split("\\")[-1].split(".")[0]
+    comic_handle = comic.split("/")[-1].split(".")[0]
     tentative_path = os.path.abspath(f'{target_folder}/{comic_handle}.json')
 
     if not force and glob.glob(tentative_path):
@@ -20,15 +27,24 @@ def main(source_folder: str, target_folder: str, force: bool) -> None:
       continue
     else:
       print(f'Creating metadata file for {comic}...')
-      comic_info = Comic(comic).input_comic_info()
+      comic_info = Comic(comic)
+
+      plt.gca().clear()
+      comic_image = mpimg.imread(comic)
+      plt.imshow(comic_image)
+      display.display(plt.gcf())
+      display.clear_output(wait=True)
+      time.sleep(1.0)
+
+      comic_info.input_comic_info()
       with open(tentative_path, 'w') as f:
-        json.dump(comic_info, f)
+        json.dump(comic_info.__dict__, f)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('-s','--source_folder', type=str, required=True, help='Folder to load metadata files from')
-  parser.add_argument('-t','--target_folder', type=str, required=True, help='Folder to save metadata files to')
-  parser.add_argument('-f', '--force', action='store_true', help='Force overwrite of existing metadata files')
+  parser.add_argument('-s','--source_folder', type=str, default="/home/jberezow/Projects/webcomic/src/assets/comics/images", help='Folder to load metadata files from')
+  parser.add_argument('-t','--target_folder', type=str, default="/home/jberezow/Projects/webcomic/src/assets/metadata/images", help='Folder to save metadata files to')
+  #parser.add_argument('-f', '--force', action='store_true', help='Force overwrite of existing metadata files')
   
   args = parser.parse_args()
   source_folder = args.source_folder
