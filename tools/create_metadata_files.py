768c8dd31dcc4f43eb50eeb89703248fd656a8a2
jberezow <jberez@live.ca>
2023-02-07 22:37:09 +0100

    Scripting path troubles

--- tools/create_metadata_files.py
+++ tools/create_metadata_files.py
@@ -13,13 +13,16 @@ from comic import Comic
 
 
 def main(source_folder: str, target_folder: str, force: bool) -> None:
+  dir_path = os.path.join(os.getcwd())
+  source_folder = os.path.join(dir_path, source_folder)
+  target_folder = os.path.join(dir_path, target_folder)
   print(f'Creating metadata files for {source_folder} and saving to {target_folder}')
 
   # Get all the files in the source folder
-  comics = glob.glob(f'{source_folder}/*.png') + glob.glob(f'{source_folder}/*.jpg')
+  comics = glob.glob(f'{source_folder}\\*.png') + glob.glob(f'{source_folder}\\*.jpg')
 
   for comic in comics:
-    comic_handle = comic.split("/")[-1].split(".")[0]
+    comic_handle = comic.split("\\")[-1].split(".")[0]
     tentative_path = os.path.abspath(f'{target_folder}/{comic_handle}.json')
 
     if not force and glob.glob(tentative_path):
@@ -32,6 +35,7 @@ def main(source_folder: str, target_folder: str, force: bool) -> None:
       plt.gca().clear()
       comic_image = mpimg.imread(comic)
       plt.imshow(comic_image)
+      plt.show()
       display.display(plt.gcf())
       display.clear_output(wait=True)
       time.sleep(1.0)
@@ -42,9 +46,9 @@ def main(source_folder: str, target_folder: str, force: bool) -> None:
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('-s','--source_folder', type=str, default="/home/jberezow/Projects/webcomic/src/assets/comics/images", help='Folder to load metadata files from')
-  parser.add_argument('-t','--target_folder', type=str, default="/home/jberezow/Projects/webcomic/src/assets/metadata/images", help='Folder to save metadata files to')
-  #parser.add_argument('-f', '--force', action='store_true', help='Force overwrite of existing metadata files')
+  parser.add_argument('-s','--source_folder', type=str, default="src\\assets\\comics\\images", help='Folder to load metadata files from')
+  parser.add_argument('-t','--target_folder', type=str, default="src\\assets\\comics\\metadata", help='Folder to save metadata files to')
+  parser.add_argument('-f', '--force', action='store_true', help='Force overwrite of existing metadata files')
   
   args = parser.parse_args()
   source_folder = args.source_folder
