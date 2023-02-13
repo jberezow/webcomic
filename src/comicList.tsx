a75842d558c137867c7af47c41179568ce6b9a7b
jberezow <jberez@live.ca>
2023-02-13 21:36:38 +0100

    Import all comics

---
+++ src/comicList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import * as fs from 'fs';
+
+interface ComicFromJson {
+  title: string;
+  date: string;
+  json_path: string;
+  comic_path: string;
+};
+
+const folderPath = './assets/comics/metadata';
+const files = fs.readdirSync(folderPath);
+
+const jsonFiles = files.filter(file => file.endsWith('.json'));
+
+const comicList: ComicFromJson[] = [];
+
+jsonFiles.forEach(file => {
+    const fileData = fs.readFileSync(`${folderPath}/${file}`);
+    comicList.push(JSON.parse(fileData.toString()));
+});
+
+export default comicList;
